f8f5b5d240c35167e28bb983cfb6c3f451c1dba7
suvasis <suvasis02@kgpian.iitkgp.ac.in>
2024-03-07 18:05:16 +0530

    cors setup is done

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express from "express";
 import userRouter from "./route/user.route";
 import postRouter from "./route/posts.route";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
